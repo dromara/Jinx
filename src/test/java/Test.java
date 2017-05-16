a5fd12a7cbd487f0f95418d79531c096ebdf2ad7
yu.xiao <yu.xiao@happylifeplat.com>
2017-05-16 09:24:53 +0800

    test

---
+++ src/test/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * <p>Description: .</p>
+ * <p>Company: 深圳市旺生活互联网科技有限公司</p>
+ * <p>Copyright: 2015-2017 happylifeplat.com All Rights Reserved</p>
+ *
+ * @author yu.xiao@happylifeplat.com
+ * @version 1.0
+ * @date 2017/5/16 9:24
+ * @since JDK 1.8
+ */
+public class Test {
+}
